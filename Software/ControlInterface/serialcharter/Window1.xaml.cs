7364fe851c4aa19f888753f33cdb0d730f8b3eac
Alistair Cheeseman <ac441@sussex.ac.uk>
2015-11-25 09:42:27 +0000

    addded simple export start stop functions

--- Software/ControlInterface/serialcharter/Window1.xaml.cs
+++ Software/ControlInterface/serialcharter/Window1.xaml.cs
@@ -15,42 +15,50 @@ using System.Windows.Threading;
 using System.IO.Ports;
 namespace serialcharter
 {
+    //class to hold data
+    public class data
+    {
+        public data(double X, decimal Y)
+        {
+            this.x = X;
+            this.y = Y;
+        }
+        public double x { get; set; }
+        public decimal y { get; set; }
+    }
     /// <summary>
     /// Interaction logic for Window1.xaml
     /// </summary>
     public partial class Window1 : Window
     {
-       static ObservableCollection<KeyValuePair<double, double>> Power = new ObservableCollection<KeyValuePair<double, double>>();
+        private const decimal offset = 0;
+        private const decimal scaleFactor = 1;
+
+        static ObservableCollection<data> Data = new ObservableCollection<data>();
         SerialPort SP;
+        DateTime Start;
         public Window1()
         {
             InitializeComponent();
-            showColumnChart();
-            initialiseserial();
+            lineChart.DataContext = Data; // set the data context for the chart
+            StartBtn.IsEnabled = true;
+            StopBtn.IsEnabled = false;
             DispatcherTimer timer = new DispatcherTimer();
-            timer.Interval = new TimeSpan(0, 0, 0,0,500);  // per 500 milliseconds, could change it
+            timer.Interval = new TimeSpan(0, 0, 0, 0, 500);  // per 500 milliseconds, could change it
             timer.Tick += new EventHandler(timer_Tick);
-            timer.IsEnabled = false; //set this to true to spawn data
+            timer.IsEnabled = true; //set this to true to spawn data
         }
-
-      static  double i = 3;
         Random random = new Random(DateTime.Now.Millisecond);
         void timer_Tick(object sender, EventArgs e)
         {
-            Power.Add(new KeyValuePair<double, double>(i, random.NextDouble()*100));
-            i += 3;
-        }
-
-        private void showColumnChart()
-        {
-            lineChart.DataContext = Power;
+            Data.Add(new data((DateTime.Now - Start).TotalMilliseconds, (decimal)random.NextDouble() * 100));
         }
         private void initialiseserial()
         {
             SP = new SerialPort();
             SP.PortName = "COM10";
             Console.WriteLine("Com port: " + SP.PortName.ToString());
-            SP.BaudRate = 2000000;
+            SP.BaudRate = 9600;
             Console.WriteLine("Baud rate: " + SP.BaudRate.ToString());
 
             Console.WriteLine("Stop bit: " + SP.StopBits.ToString());
@@ -59,40 +67,68 @@ namespace serialcharter
             SP.Handshake = Handshake.XOnXOff;
             SP.DataReceived += sp_DataReceived;
             SP.Open();
-
             int test = SP.BaseStream.ReadByte();
             Console.WriteLine(test.ToString());
         }
-        static string assembling;
         private void sp_DataReceived(object sender, SerialDataReceivedEventArgs e)
         {
             SerialPort sep = (SerialPort)sender;
             string indata = sep.ReadExisting();
-            Console.Write(indata);
-            string[] data;
+            string[] buffer;
 
-   data = indata.Split(Environment.NewLine.ToCharArray(),StringSplitOptions.RemoveEmptyEntries);
-   foreach (string val in data)
-   {
-      Dispatcher.Invoke(DispatcherPriority.Normal, (Action)(() =>
-       {
-           double value;
-           if (double.TryParse(val,out value))
-           {   Power.Add(new KeyValuePair<double, double>(i,value));
-               i += 3;}
-            
-             
-          
-       }));    
-     
-   }
-
+            buffer = indata.Split(Environment.NewLine.ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
+            foreach (string val in buffer)
+            {
+                Dispatcher.Invoke(DispatcherPriority.Normal, (Action)(() =>
+                 {
+                     double value;
+                     if (double.TryParse(val, out value))
+                     {
+                         decimal cmH20 = (((decimal)value - offset) * scaleFactor);
+                         Data.Add(new data((DateTime.Now - Start).TotalMilliseconds, cmH20));
+                     }
+                 }));
+            }
         }
-      
         private void Window_Closed(object sender, EventArgs e)
         {
+            if (SP.IsOpen == true)
             SP.Close();
         }
 
+        private void StartBtn_Click(object sender, RoutedEventArgs e)
+        {
+            Data = new ObservableCollection<data>();
+            Start = DateTime.Now;
+            initialiseserial();
+            StopBtn.IsEnabled = true;
+            StartBtn.IsEnabled = false;
+
+        }
+
+        private void StopBtn_Click(object sender, RoutedEventArgs e)
+        {
+            SP.Close();
+            StartBtn.IsEnabled = true;
+            StopBtn.IsEnabled = false;
+        }
+
+        private void ExportBtn_Click(object sender, RoutedEventArgs e)
+        {
+            
+            if (System.IO.File.Exists("data.csv"))
+            {
+                System.IO.File.Delete("data.csv");
+            }
+            string writebuffer = string.Empty;
+            writebuffer += "Milliseconds, Value\n";
+            foreach (data d in Data)
+            {
+
+                writebuffer += d.x.ToString() + ", " + d.y.ToString() + "\n";
+            }
+            System.IO.File.WriteAllText("data.csv", writebuffer);
+            MessageBox.Show(String.Format("Export Complete, {0} entries saved", Data.Count()));
+        }
     }
 }
