3567155663c511a5dfa1510f8e8c8212d74cda8a
Alistair Cheeseman <ali@kmxsoftware.co.uk>
2015-11-27 12:46:06 +0000

    updated pressure transducer code

--- Firmware/ArduinoTests/PressureTransducer/PressureTransducer.ino
+++ Firmware/ArduinoTests/PressureTransducer/PressureTransducer.ino
@@ -2,21 +2,30 @@
 
 void setup()
 {
+delay(1000);
+delay(1000);  
   Wire.begin();        // join i2c bus (address optional for master)
   Serial.begin(9600);  // start serial for output
+  //Wire.setClock(400000L);
 }
 
 void loop()
 {
   uint16_t val = 0x0000;
-  Wire.requestFrom(0x28, 16);  
-
-  while(Wire.available())    // slave may send less than requested
-  { 
+  Wire.requestFrom(0x28, 2);
+  while(Wire.available())
+  {
     val = (Wire.read()<<8); // receive a byte as character
-    val = val | Wire.read(); // receive a byte as character
+    val = val | Wire.read(); // receive a byte as character  
+    }  
+  uint8_t sensstatus  = (val >> 14);
+  if (sensstatus)
+  {
+  Serial.println("Error");
+  Serial.println(sensstatus, BIN);
   }
+  
   Serial.println(val,DEC);
 
-  delay(10);
+  delay(100);
 }
