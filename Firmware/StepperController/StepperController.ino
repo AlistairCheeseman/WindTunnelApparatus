d1921ceaaf9787e3e1e80b885747ba59bbf6fbd8
Alistair Cheeseman <ac441@sussex.ac.uk>
2016-02-04 13:05:15 +0000

    updated pins to match schematics updated delay variables to set max speed and min speed added initial command structure tidied serial init routine and default to wave drive stepper converted to microsecond delays rather than millisecond delays for greater accn control fixed right turn only bug tidied step routine added stop motor routine

--- Firmware/StepperController/StepperController.ino
+++ Firmware/StepperController/StepperController.ino
@@ -1,61 +1,51 @@
-/* stepper motor pins */
-int XM1 = 9;
-int XM2 = 11;
-int XM3 = 10;
-int XM4 = 12;
-int YM1 = 5;
-int YM2 = 7;
-int YM3 = 6;
-int YM4 = 8;
-/* Limit switches */
-int LimX = A0;
-int LimY = A1;
-/* delay between each step change */
-const int delayTime = 3;
-/* direction mode */
-int dirX;
-int dirY;
+/* stepper motor pins, NOTE ORDER: A1 B1 A2 B2 */
+uint8_t XM1 = 9, XM2 = 11, XM3 = 10, XM4 = 12;
+uint8_t YM1 = A0, YM2 = A2, YM3 = A1, YM4 = A4; // use analog pins for Y axis.
+/* Limit switches, make sure to check at what end of end stop*/
+uint8_t LimX = 7, LimY = 8;
+/* min delay between each step change */
+uint16_t minDelayTime = 1000, maxDelayTime = 5000;
+/* what direction each motor rotates in */
+uint8_t dirX, dirY;
 #define DIR_LEFT 1
 #define DIR_RIGHT 2
-/* step mode */
-int modeX;
-int modeY;
+/* what mode the stepper motor is controlled as */
+uint8_t modeX, modeY;
 #define STEP_WAVE 1
 #define STEP_FULL 2
 #define STEP_HALF 3
-uint8_t lastwavestepX;
-uint8_t lastwavestepY;
-/* positional status */
-unsigned int stepStatusX;
-unsigned int stepStatusY;
+
+/* current phase/output of the stepper*/
+uint8_t stepStatusX, stepStatusY;
+/* if half stepping, we need some extra variables - the previous state */
+uint8_t lastHalfStepX, lastHalfStepY;
+
+/* command max length */
+// MOTOR MODE SPEED DIRECTION DISTANCE
+// 1      1    1       1        2 + 1 = 7
+#define MAX_COMMAND_LENGTH 7 
 
 
-void zero();
-void stepX();
-void stepY();
-uint8_t fourBitShift(uint8_t val);
+/* prototypes */
+void zero();  // rotate each motor to one end until the limit switch is hit, now we know the posn.
+void stepX(); // make the Xmotor step
+void stepY(); // make the Ymotor step
+uint8_t fourBitShiftRight(uint8_t val); // barrel shift right
+uint8_t fourBitShiftLeft(uint8_t val);  // barrel shift left
+void stopMotorX(); // disable output -- stops motor from being active
+void stopMotorY(); // disable output -- stops motor from being active
+
+// step program initialises serial connection and Zeroes (no hold) axes.
 void setup() {
   Serial.begin(9600);
-
-
-
-Serial.println("HELLO, PLEASE ENTER CONFIG SETTINGS: X1A X1B X2A X2B Y1A Y1B Y2A Y2B LIMITX LIMITY");
-/* dont need this for time being */
-/*
-while (Serial.available() ==0) {}
-  XM1 = Serial.parseInt();
-  XM2 = Serial.parseInt();
-  XM3 = Serial.parseInt();
-  XM4 = Serial.parseInt();
-  YM1 = Serial.parseInt();
-  YM2 = Serial.parseInt();
-  YM3 = Serial.parseInt();
-  YM4 = Serial.parseInt();
-  LimX = Serial.parseInt();
-  LimY = Serial.parseInt();
- */
- if ((XM1 +XM2 + XM3 + XM4) > 0 && (YM1 +YM2 + YM3 + YM4) > 0 && (LimX + LimY) > 0)
-    Serial.println("OK");  
+  Serial.println("HELLO, PLEASE ENTER CONFIG SETTINGS: MINDELAY (=1) MAXDELAY (=10)");
+  while (Serial.available() ==0) {} // wait for serial data
+  minDelayTime = Serial.parseInt();
+  maxDelayTime = Serial.parseInt();
+  //sanity check that configuration is ok.
+ if (minDelayTime > 0)
+    Serial.println("OK WAIT");  
+ //Setup ports
   pinMode(XM1, OUTPUT);
   pinMode(XM2, OUTPUT);
   pinMode(XM3, OUTPUT);
@@ -64,22 +54,23 @@ while (Serial.available() ==0) {}
   pinMode(YM2, OUTPUT);
   pinMode(YM3, OUTPUT);
   pinMode(YM4, OUTPUT);
-  
   pinMode(LimX, INPUT);
   pinMode(LimY, INPUT);
-  /* set pullups */
+  /* set pullups on limit switches */
   digitalWrite(LimX, HIGH);
   digitalWrite(LimY, HIGH);
 
   /* intialise all motor states */
-    modeX = STEP_WAVE;
-  modeY = STEP_WAVE;
-  stepStatusX = 0B0011; // 3 for FULL step and 1 for WAVE/HALF Step
-  stepStatusY = 3; // 3 for FULL step and 1 for WAVE/HALF Step
-  lastwavestepX = 0B1001;
-    lastwavestepY = 0B1001;
+  modeX = STEP_HALF; // default to half wave drive
+  modeY = STEP_HALF; // default to half wave drive
+  // initialise motor states
+  lastHalfStepX = 0B1001; stepStatusX = 0B0011;
+  lastHalfStepY = 0B1001; stepStatusY = 0B0011; 
+  // zero the aixs
   Serial.println("ZEROING ALL AXES");
   zero();
+  // tell the program we are ready to take commands
+  Serial.println("OK READY");
 }
 
 void zero()
@@ -87,51 +78,41 @@ void zero()
   // for each axis step all the way until a limit is met.
   dirX = DIR_RIGHT;
   dirY = DIR_LEFT;
-  Serial.println(stepStatusX);
+  //while limit switch is not triggered, keep stepping.
   while (digitalRead(LimX) == 1)
   {
-    stepX();
-    Serial.println(stepStatusX);
-    delay(delayTime);
+    stepX(); // step
+    delayMicroseconds(minDelayTime); // wait min delay time
   }
-  Serial.println("X ZEROED");
+  stopMotorX();
   while (digitalRead(LimY) == 1)
   {
     stepY();
-    delay(delayTime);
+    delayMicroseconds(minDelayTime);
   }
+  stopMotorY();
 }
 
 uint8_t fourBitShiftRight(uint8_t val)
 {
-  return   ((((val & 0B1000) >> 3) | (val << 1 )) & 0B1111);
+  return   (((val & 0B1000) >> 3) | (val << 1 )) & 0B1111; // take the left hand digit, insert it on the right.
 }
 uint8_t fourBitShiftLeft(uint8_t val)
 {
-  return   ((((val & 0B1000) << 3) | (val >> 1 )) & 0B1111);
+  return   (((val & 0B0001) << 3) | (val >> 1 )) & 0B1111; // take the right hand digit and insert it on the left.
 }
 
 void stepX()
 {
-  if (modeX == STEP_HALF)
+  if (modeX == STEP_HALF)// if half stepping, use a different routine
   {
-     uint8_t currentstep = 0;
-      if (stepStatusX == 2 || stepStatusX == 4 || stepStatusX == 8 || stepStatusX == 1)
-      {// power of 2
-        if (dirX == DIR_LEFT)
-         currentstep =  fourBitShiftRight(lastwavestepX);
-        else
-         currentstep =  fourBitShiftLeft(lastwavestepX);
-      }
-      else
-      { 
-        if (dirX == DIR_LEFT)
-          currentstep = fourBitShiftLeft(lastwavestepX);
-        else
-          currentstep = fourBitShiftRight(lastwavestepX);
-      }
-      lastwavestepX = stepStatusX;
-      stepStatusX = currentstep;
+     uint8_t currentstep = 0; // variable to hold new step
+     if (dirX == DIR_LEFT)
+      currentstep = fourBitShiftLeft(lastHalfStepX); // perform the shift on the previous state
+     else
+      currentstep = fourBitShiftRight(lastHalfStepX);
+     lastHalfStepX = stepStatusX; // put the current state into the previous state variable
+     stepStatusX = currentstep; // load up the new current state
   }
   else
   {
@@ -141,7 +122,7 @@ void stepX()
     else
       stepStatusX = fourBitShiftRight(stepStatusX);
   }
-  
+  // and output the new motor poles
 digitalWrite(XM1, (stepStatusX & 0B1000) >> 3);
 digitalWrite(XM2, (stepStatusX & 0B0100) >> 2);
 digitalWrite(XM3, (stepStatusX & 0B0010) >> 1);
@@ -149,19 +130,171 @@ digitalWrite(XM4, (stepStatusX & 0B0001));
 }
 void stepY()
 {
-  // update the next output and output it.
-if (dirY == DIR_LEFT)
-  stepStatusY = (((stepStatusY & 0B1000) >> 3) | (stepStatusY << 1 ));
-else
-  stepStatusY = (((stepStatusY & 0B1) << 3) | (stepStatusY >> 1));
-
-stepStatusY = stepStatusY & 0B1111;
+  if (modeY == STEP_HALF) // if half stepping use a different routine
+  {
+     uint8_t currentstep = 0; // variable to hold new step
+     if (dirY == DIR_LEFT)
+      currentstep = fourBitShiftLeft(lastHalfStepY); // perform shift on previous state
+     else
+      currentstep = fourBitShiftRight(lastHalfStepY);
+     lastHalfStepY = stepStatusY; // put the current state into the last state
+     stepStatusY = currentstep; // update the output
+  }
+  else
+  {
+    // WAVE STEP AND FULL STEP, Simply shift output.
+    if (dirY == DIR_LEFT)
+      stepStatusY = fourBitShiftLeft(stepStatusY);
+    else
+      stepStatusY = fourBitShiftRight(stepStatusY);
+  }
+  // output the new motor state
 digitalWrite(YM1, (stepStatusY & 0B1000) >> 4);
 digitalWrite(YM2, (stepStatusY & 0B0100) >> 3);
 digitalWrite(YM3, (stepStatusY & 0B0010) >> 2);
 digitalWrite(YM4, (stepStatusY & 0B0001) >> 1);
 }
-void loop() {
- while (Serial.available() ==0) {}
- 
+void stopMotorX()
+{
+  // If motor state is left, the coils will remain energised so we stop it.
+  digitalWrite(XM1, LOW);
+  digitalWrite(XM2, LOW);
+  digitalWrite(XM3, LOW);
+  digitalWrite(XM4, LOW);
 }
+void stopMotorY()
+{
+  // If motor state is left, the coils will remain energised so we stop it.
+  digitalWrite(YM1, LOW);
+  digitalWrite(YM2, LOW);
+  digitalWrite(YM3, LOW);
+  digitalWrite(YM4, LOW);
+}
+void loop() {
+  byte * command = (byte*) malloc(MAX_COMMAND_LENGTH);
+  // initialise to zero
+  for (uint8_t val = 0; val <= (MAX_COMMAND_LENGTH - 1); val ++)
+    command[val] = 0x00;
+  uint8_t hasWholeCommand = 0;
+  uint8_t charCount = 0;
+  while(0 == hasWholeCommand)
+  {
+    while(0 == Serial.available()) {} // wait for at least one byte of data.
+    byte charIn = Serial.read();
+    if ('\r' != charIn &&  '\n' != charIn)
+    {
+      command[charCount] = charIn;
+      charCount++;
+    }
+    if (0xA == charIn)
+      hasWholeCommand = 1; //set flag to show we have whole byte 
+  }
+  if (command[0] == 0x00)
+    return; // the command is blank, quit this loop.
+/*
+ * ACTUALLY PROCESS THE COMMAND
+ */
+  // get the total number of revs and steps requested.
+uint16_t revs = (command[4] << 8) | command[5];
+uint8_t steps = command[6];
+  
+  if (command[0] == 0x1) // motorX
+  {
+     if (command[3] == 0x1) // LEFT
+      dirX = DIR_LEFT;
+     else if (command[3] == 0x2) // RIGHT
+      dirX = DIR_RIGHT;
+
+    if (command[1] == 0x1) // HALF
+      modeX = STEP_HALF;
+    else if (command[1] == 0x2) //WAVE  
+      modeX = STEP_WAVE;
+    else if (command[1] == 0x2) //FULL  
+      modeX = STEP_FULL;
+
+    //actually step the motor
+     if (command[2] == 0x1) // FAST
+   for ( uint16_t rev = 0; rev <= revs; rev++)
+   {
+    stepX();
+    delayMicroseconds(minDelayTime);
+   }
+  else if (command[2] == 0x2) // SLOW  
+   for ( uint16_t rev = 0; rev <= revs; rev++)
+   {
+    stepX();
+    delayMicroseconds(maxDelayTime);
+   }
+  else if (command[2] == 0x2) // DYNAMIC
+  {
+   uint32_t totalStep = (revs * 200) + steps;
+   uint16_t currentDelay = maxDelayTime;
+   uint16_t accn = 933.34;
+   uint16_t newdelay = 0;
+   for ( uint16_t rev = 0; rev <= revs; rev++)
+   {
+    if (newdelay > 1200)
+      newdelay = 1/(66.66 + (millis() * accn * 1000));
+     stepX();
+     delayMicroseconds(currentDelay);
+   }
+  }
+
+
+
+      
+  }
+  else if (command[0] == 0x2) //motorY
+  {
+     if (command[3] == 0x1) // LEFT
+      dirY = DIR_LEFT;
+     else if (command[3] == 0x2) // RIGHT
+      dirY = DIR_RIGHT;
+
+    if (command[1] == 0x1) // HALF
+      modeY = STEP_HALF;
+    else if (command[1] == 0x2) //WAVE  
+      modeY = STEP_WAVE;
+    else if (command[1] == 0x2) //FULL  
+      modeY = STEP_FULL;
+
+
+          //actually step the motor
+     if (command[2] == 0x1) // FAST
+   for ( uint16_t rev = 0; rev <= revs; rev++)
+   {
+    stepY();
+    delayMicroseconds(minDelayTime);
+   }
+  else if (command[2] == 0x2) // SLOW  
+   for ( uint16_t rev = 0; rev <= revs; rev++)
+   {
+    stepY();
+    delayMicroseconds(maxDelayTime);
+   }
+  else if (command[2] == 0x2) // DYNAMIC
+  {
+   uint32_t totalStep = (revs * 200) + steps;
+   uint16_t currentDelay = maxDelayTime;
+   uint16_t accn = 933.34;
+   uint16_t newdelay = 0;
+   for ( uint16_t rev = 0; rev <= revs; rev++)
+   {
+    if (newdelay > 1200)
+      newdelay = 1/(66.66 + (millis() * accn * 1000));
+     stepY();
+     delayMicroseconds(currentDelay);
+   }
+  }
+
+  }
+ 
+
+ 
+  
+ 
+      
+free(command);
+  Serial.println("OK DONE"); 
+}
+
