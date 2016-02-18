bb60108a44c3a0592f984c71e2b37816077beaf1
Alistair Cheeseman <ac441@sussex.ac.uk>
2016-02-18 23:29:26 +0000

    increased serial speed to 2M - found anything slower would cause buffer overflow issues

--- Firmware/PressureTransducerArray/PressureTransducerArray.srcs/sources_1/new/uart_TX.vhd
+++ Firmware/PressureTransducerArray/PressureTransducerArray.srcs/sources_1/new/uart_TX.vhd
@@ -52,6 +52,7 @@ architecture Behavioral of UART_TX_CTRL is
 type TX_STATE_TYPE is (RDY, LOAD_BIT, SEND_BIT);
 
 --2Mbaud --> 49 -- 100MHz / 2Mbaud -1 =  50 -1 = 49 =    00000000110001
+--constant BIT_TMR_MAX : std_logic_vector(13 downto 0) := "10100010110000"; --10416 = (round(100MHz / 9600)) - 1
 constant BIT_TMR_MAX : std_logic_vector(13 downto 0) := "00000000110001"; --2Mbaud
 constant BIT_INDEX_MAX : natural := 10;
 
