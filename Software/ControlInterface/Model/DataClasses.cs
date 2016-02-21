1d0cc9eb5523c719f7f5ba4a010f7343bc062db5
Alistair Cheeseman <ac441@sussex.ac.uk>
2016-02-21 10:01:46 +0000

    better defined motor enums

--- Software/ControlInterface/Model/DataClasses.cs
+++ Software/ControlInterface/Model/DataClasses.cs
@@ -12,10 +12,10 @@ namespace Model
     /// 
 
         /* MOTOR ENUMS */
-    public enum MotorDirection { none, left, right }
-    public enum MotorSpeed { none, slow, fast, dynamic }
-    public enum MotorAxis { none, x, y }
-    public enum MotorStep { none, wave, half, full }
+    public enum MotorDirection { none = 0, left = 1, right = 2 }
+    public enum MotorSpeed { none = 0, fast = 1, slow = 2, dynamic = 3 }
+    public enum MotorAxis { none = 0, x = 1, y = 2 }
+    public enum MotorStep { none = 0, wave = 1, half = 2, full = 3 }
 
     /*ViewModel Connection States */
     public enum ConnectionState {  none, ConnectedREADY, ConnectedINIT, ConnectedBusy}
